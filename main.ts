9083a4251ee898c8fdcf5715b47d579f27ceb42a
1711004 <104405654+1711004@users.noreply.github.com>
2022-06-02 10:11:29 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
+music.setVolume(255)
+basic.showString("Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you")
 basic.forever(function () {
-	
+    music.playTone(440, music.beat(BeatFraction.Half))
+    basic.pause(200)
+    music.playTone(440, music.beat(BeatFraction.Half))
+})
+basic.forever(function () {
+    serial.writeLine("" + (sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+    )))
+    basic.showNumber(sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+    ))
 })
